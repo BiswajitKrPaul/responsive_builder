ebc9c13c835234d6fa06ee1516831aeae81720d0
Biswajit Paul <biswajitkmrpl46@gmail.com>
2022-02-25 03:36:35 +0530

    Extension methods added for route.

--- lib/src/mediaquery_extensions.dart
+++ lib/src/mediaquery_extensions.dart
@@ -27,13 +27,43 @@ extension CustomExtension on BuildContext {
   double getHeight(double percentage) => (screenHeight * percentage) / 100;
 
   /// returns the passed percentage width
-  double getWidth(double percentage) => (screenHeight * percentage) / 100;
+  double getWidth(double percentage) => (screenWidth * percentage) / 100;
 
   /// returns the MediaQueryData object for legacy data
   MediaQueryData get mediaQuery => MediaQuery.of(this);
 
-  //Todo not sure if it will work or not
+  Object? get arguments => ModalRoute.of(this)!.settings.arguments;
+
   /// pushNamedRoute
-  navigator(String route, {Object? arguments}) =>
-      Navigator.of(this).pushNamed(route, arguments: arguments);
+  Future<T?> pushNamed<T extends Object?>(String route, {Object? arguments}) =>
+      Navigator.pushNamed(this, route, arguments: arguments);
+
+  /// popAndPushNamed
+  Future<T?> popAndPushNamed<T extends Object?, TO extends Object?>(
+    String route, {
+    Object? arguments,
+    TO? result,
+  }) =>
+      Navigator.popAndPushNamed(
+        this,
+        route,
+        arguments: arguments,
+        result: result,
+      );
+
+  /// pushNamedRoute
+  Future<T?> pushReplacementNamed<T extends Object?, TO extends Object?>(
+    String route, {
+    Object? arguments,
+    TO? result,
+  }) =>
+      Navigator.pushReplacementNamed(
+        this,
+        route,
+        arguments: arguments,
+        result: result,
+      );
+
+  /// pop Route
+  void pop<T extends Object?>({T? result}) => Navigator.pop(this, result);
 }
