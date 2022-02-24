8f461a096b8acccf9c16761ab800ce476f78da83
Biswajit Paul <biswajitkmrpl46@gmail.com>
2022-02-25 03:50:49 +0530

    name changes done

--- lib/src/responsive_builder.dart
+++ lib/src/responsive_builder.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:responsive_builder_ui/src/mediaquery_extensions.dart';
 
 /// A Widget responsible for giving the layout options for `Mobile` and `Tablet`
-class ResponsiveBuilder extends StatelessWidget {
+class ResponsiveBuilderUI extends StatelessWidget {
   /// Gives the builder widget for `Mobile`. Required parameter
   final Widget Function(BuildContext context) mobileBuilder;
 
@@ -10,7 +10,7 @@ class ResponsiveBuilder extends StatelessWidget {
   ///
   /// It is optional parameter,if not provided then `[mobileBuilder]` will be used.
   final Widget Function(BuildContext context)? tabletBuilder;
-  const ResponsiveBuilder({
+  const ResponsiveBuilderUI({
     Key? key,
     required this.mobileBuilder,
     this.tabletBuilder,
